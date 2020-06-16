8f987163c6eded4316bd3354e1837bc9885686e3
Rradley <34035184+Rradley@users.noreply.github.com>
2020-06-16 21:34:29 +0300

    Add files via upload

---
+++ setup_pipeline.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+sudo chmod 666 /var/run/docker.sock
+pip install cwltool==3.0.20200530110633
+mkdir data/
+wget ./data/ https://sra-download.ncbi.nlm.nih.gov/traces/sra79/SRR/011381/SRR11654257
+
+git clone --depth=50 https://github.com/EBI-Metagenomics/ebi-metagenomics-cwl.git ebi-metagenomics-cwl
+git clone -b rebuild_dockers https://github.com/EBI-Metagenomics/pipeline-v5
+
+cp ./assembly.yml /pipeline-v5/
+cp ./wgs.yml /pipeline-v5/
+cp ./run.sh /pipeline-v5/
+
+
+exec bash
