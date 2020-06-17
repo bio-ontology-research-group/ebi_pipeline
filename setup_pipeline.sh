83543f676dd2a3d3960058d59a9375b3e4554d74
Rradley <34035184+Rradley@users.noreply.github.com>
2020-06-17 12:33:38 +0300

    Update setup_pipeline.sh

--- setup_pipeline.sh
+++ setup_pipeline.sh
@@ -3,7 +3,13 @@
 sudo chmod 666 /var/run/docker.sock
 pip3 install cwltool==3.0.20200530110633
 mkdir data/
+
+
+sudo apt-get --quiet install --yes libxml-libxml-perl
+wget https://ftp-trace.ncbi.nlm.nih.gov/sra/sdk/2.10.7/sratoolkit.2.10.7-ubuntu64.tar.gz
+tar xzvf sratoolkit.2.10.7-ubuntu64.tar.gz
 wget ./data/ https://sra-download.ncbi.nlm.nih.gov/traces/sra79/SRR/011381/SRR11654257
+
 
 git clone --depth=50 https://github.com/EBI-Metagenomics/ebi-metagenomics-cwl.git ebi-metagenomics-cwl
 git clone -b rebuild_dockers https://github.com/EBI-Metagenomics/pipeline-v5
@@ -11,9 +17,8 @@ git clone -b rebuild_dockers https://github.com/EBI-Metagenomics/pipeline-v5
 
 #./download_dbs.sh
 
-cp ./assembly.yml /pipeline-v5/
-cp ./wgs.yml /pipeline-v5/
-cp ./run.sh /pipeline-v5/
-
+cp ./assembly.yml pipeline-v5/
+cp ./wgs.yml pipeline-v5/
+cp ./run.sh pipeline-v5/
 
 exec bash
